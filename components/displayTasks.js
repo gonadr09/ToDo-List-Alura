e65efd9da58dcc79dd57b81b48fccfffecad7429
gonadr09 <gonzalo_diaz09@hotmail.com>
2022-07-08 18:22:54 -0300

    test tasks 3

--- components/displayTasks.js
+++ components/displayTasks.js
@@ -18,19 +18,19 @@ export const displayTasks = () => {
             task: "Seguir en las redes a este programador 😋 (tarea de prueba)",
             date: moment().format("DD/MM/YYYY"),
             completed: false,
-            id: "d3c2f6f1-6711-4723-ab19-7b28c23b894a",
+            id: "d3c2f6f1-6711-4723-ab19-7b28c23b894b",
           },
           {
             task: "Hacer las compras 🥱 (tarea de prueba)",
-            date: moment().subtract(1, 'days').format("DD/MM/YYYY"),
+            date: moment().add(1, 'days').format("DD/MM/YYYY"),
             completed: true,
-            id: "8af215c8-6739-40a6-ae62-3f07d68a71f7",
+            id: "8af215c8-6739-40a6-ae62-7b28c23b894c",
           },
           {
             task: "Reservar turno dentista 👨‍⚕️ (tarea de prueba)",
             date: moment().subtract(1, 'days').format("DD/MM/YYYY"),
             completed: false,
-            id: "533715d1-c61e-4b53-8de7-3bd9925ffd5e",
+            id: "533715d1-c61e-4b53-8de7-7b28c23b894d",
           },
         ];
         localStorage.setItem('tasks', JSON.stringify(tasksListStorage));
